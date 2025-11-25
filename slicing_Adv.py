a29c53b5b723cf50a613425b920eaf19f2545acf
1707jai <jhind1546@gmail.com>
2025-11-25 09:15:25 +0000

    added slicing_Adv.py

---
+++ slicing_Adv.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+arr1=np.arange(1,17).reshape(4,4)
+print("\n")
+print(arr1)
+
+print("\n",arr1[1, :])  #print 2nd row
+
+print("\n",arr1[ : ,2])  #print 3rd column
+
+print("\n",arr1[2: , :]) #print last 2 row
+
+print("\n",arr1[:, :2])  #print last 2 column
+
+print("\n",arr1[ ::-1 , :]) # reverse rows 
+
+print("\n",arr1[ :, ::-1 ]) # reverse columns
+
+print("\n",arr1[1:3,1:3])  # print 2*2 block from middle 
+
+
+arr2=np.arange(1,17).reshape(4,4)
+
+print("\n",arr2[ 0, : ]) #print 1rd row 
+
+print("\n",arr2[ :, 0])  #print 3rd row
+
+print("\n",arr2[ 2, :]) #print 2nd row
+
+print("\n",arr2[:, 0]) #print 1 column
+
+print("\n",arr2[:, 2]) #print 3rd column
+
+print("\n",arr2[2:, :]) #print last 2nd row
+
+print("\n",arr2[::-1 , ::-1]) #reverse whole matrix
